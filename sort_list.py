68a89ec08e26f13791e020352f9dc11c9903fd7d
wulandarikaranamu <wulandarikaranamu02@gmail.com>
2022-01-05 08:43:31 +0700

    oii

---
+++ sort_list.py
@@ -0,0 +1,8 @@
+#fungsi sort dalam list
+
+data = [100,95,1,3,56,78,854,2,5]
+data.sort()#tidak perlu ada parameter yg kita tulis dlm fungsi
+print(data)
+
+#sort digunakan untuk mengurutkan list yang acak
+#dimana sort mengurutkan nilai dari terkecil hingga terbesar
